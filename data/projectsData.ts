cff4334742876cb22dfb1c271480a95fda4423c4
j4ckrisz <frenshystyle255@protonmail.com>
2024-06-21 00:37:06 -0500

    edit 1

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -6,22 +6,29 @@ interface Project {
 }
 
 const projectsData: Project[] = [
+  
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Password Generator Website',
+    description: 'Is a website where you can create a custom password based on your needs. The website is created with HTML, CSS and JavaScript',
+    href: 'https://p4sswdg3n.netlify.app',
+    imgSrc: '/static/images/p4sswdg3n.png',
   },
+
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Wi-Fi Penetration Testing Tool',
+    description: "WiSec is tool that allows yo to do most of the WPA2-PSK and WEP encryption attacks. The tool let's you crack previous files that you have captured using crunch or a wordlist.",
+    href: 'https://github.com/j4ckrisz/wisec',
+    imgSrc: '/static/images/wisec_photo_bash.png',
   },
+
+  {
+    title: 'Arduino Projects Repository',
+    description: "This repository have all the arduino projects that I created in my occupational class (Pre-Engineering with concentration in electronics)",
+    href: 'https://github.com/j4ckrisz/Arduino_Projects',
+    imgSrc: '',
+  }
+
+
 ]
 
 export default projectsData
